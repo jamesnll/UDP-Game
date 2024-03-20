bd879f0963b2d690880db9c68b1db80a74f26885
jamesnll <langillejames11@gmail.com>
2024-03-20 11:00:07 -0700

    Implemented ncurses library

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include "../include/display.h"
+#include <ncurses.h>
 #include <stdlib.h>
 
 int main(void)
 {
+    initscr();
+    addstr("-----------------\n| codedrome.com |\n| ncurses Demo  |\n-----------------\n\n");
+    refresh();
+
+    addstr("\npress any key to exit...");
+    refresh();
+
+    getch();
+
+    endwin();
     display("Hello, World");
 
     return EXIT_SUCCESS;
