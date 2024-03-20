0deb79de2ae8be8227fec77ef5d5e377fc3d6e89
jamesnll <langillejames11@gmail.com>
2024-03-20 12:01:37 -0700

    Added code to get arrow keys as user input

--- src/main.c
+++ src/main.c
@@ -4,17 +4,41 @@
 
 int main(void)
 {
+    int ch;
     initscr();
-    addstr("-----------------\n| codedrome.com |\n| ncurses Demo  |\n-----------------\n\n");
-    refresh();
+    cbreak();                // Disable line buffering
+    noecho();                // Don't echo user input to the screen
+    keypad(stdscr, TRUE);    // Enable keypad mode to capture arrow keys
+    // Print a prompt
+    printw("Press an arrow key (or q to quit): ");
+    refresh();    // Refresh the screen to display the prompt
 
-    addstr("\npress any key to exit...");
-    refresh();
+    while((ch = getch()) != 'q')
+    {
+        // Handle arrow keys
+        switch(ch)
+        {
+            case KEY_UP:
+                mvprintw(1, 0, "You pressed: Up arrow");
+                break;
+            case KEY_DOWN:
+                mvprintw(1, 0, "You pressed: Down arrow");
+                break;
+            case KEY_LEFT:
+                mvprintw(1, 0, "You pressed: Left arrow");
+                break;
+            case KEY_RIGHT:
+                mvprintw(1, 0, "You pressed: Right arrow");
+                break;
+            default:
+                mvprintw(1, 0, "You pressed: %c", ch);
+                break;
+        }
+        refresh();    // Refresh the screen to display the input
+    }
 
-    getch();
-
+    // End ncurses
     endwin();
-    display("Hello, World");
 
     return EXIT_SUCCESS;
 }
